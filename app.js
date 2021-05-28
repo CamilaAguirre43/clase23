117702fb523f9e24fba90d106f20787fc1d7ae11
CamilaAguirre43 <camilaaguirre458@gmail.com>
2021-05-28 15:23:42 -0300

    levanta servidor

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express(); // se refiere a la ejecucion de una funcion
+const port = 3000;
+
+
+app.listen(port, () => console.log('Servidor corriendo en el puerto ' + port)) // 3000 es el numero de puerto - con el .listen levantamos un servidor
+
+
