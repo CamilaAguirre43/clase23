d0ae29eea404096af12c95299af4a7ae06807c9a
CamilaAguirre43 <camilaaguirre458@gmail.com>
2021-05-28 15:30:17 -0300

    Comentarios

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express(); // se refiere a la ejecucion de una funcion
 const port = 3000;
 
 
-app.listen(port, () => console.log('Servidor corriendo en el puerto ' + port)) // 3000 es el numero de puerto - con el .listen levantamos un servidor
+app.listen(port, () => console.log('Servidor corriendo en el puerto ' + port)) // 3000 es el numero de puerto - con el .listen levantamos un servidor / va un callback en forma de arrow function tambien
+
+
 
 
